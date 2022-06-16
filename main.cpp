55cc9cfbae752efcbbb82601549b0c6a1ab03129
DmitriyVypovskoi <davypovskoy@miem.hse.ru>
2022-06-16 03:52:34 +0300

    v4

--- main.cpp
+++ main.cpp
@@ -24,6 +24,7 @@ int snake_position_y = field_size_y / 2;
 int snake_length = 4;
 int snake_direction = SNAKE_DIRECTION_RIGHT;
 int score = 0;
+
 bool game_paused = false;
 bool game_over = false;
 
@@ -328,8 +329,6 @@ int main()
 
     while(window.isOpen()) {
         sf::Event event;
-
-
 
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
